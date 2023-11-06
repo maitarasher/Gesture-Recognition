44323f7b7a16f64c6f0b8ecc8bf8a1af51e5036a
elvinawibisono <66840764+elvinawibisono@users.noreply.github.com>
2023-11-05 20:28:28 -0500

    add CMakeLists.txt

---
+++ build/CMakeFiles/gesture_project.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gesture_project.dir/samples/opencv_version.cpp.o"
+  "CMakeFiles/gesture_project.dir/samples/opencv_version.cpp.o.d"
+  "gesture_project"
+  "gesture_project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gesture_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ samples/opencv_version.cpp
@@ -0,0 +1,7 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+int main() {
+    std::cout << "OpenCV version: " << CV_VERSION << std::endl;
+    return 0;
+}
