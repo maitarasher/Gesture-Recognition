33cbd8f827b01d02360c781bda6836be6d0385ba
Yana Botvinnik <yb2462@barnard.edu>
2023-11-28 17:39:05 -0500

    fixed and tested the data_classes

--- src/data_classes.hpp
+++ src/data_classes.hpp
@@ -22,14 +22,16 @@ using namespace std;
  * @float y
  * @float z
  */
+
+//TESTED
 class Landmark {
 public:
-    double x;
-    double y;
-    double z;
+    float x;
+    float y;
+    float z;
 Landmark(): x(0.0), y(0.0), z(0.0) {}
-Landmark(double x, double y, double z) : x(x), y(y), z(z) {}
-Landmark(const std::vector<double>& point) {
+Landmark(float x, float y, float z) : x(x), y(y), z(z) {}
+Landmark(vector<float>& point) {
         if (point.size() == 3) {
             x = point[0];
             y = point[1];
@@ -39,25 +41,22 @@ Landmark(const std::vector<double>& point) {
             x = 0.0;
             y = 0.0;
             z = 0.0;
-            std::cerr << "Warning: Invalid vector size for Landmark constructor. Default values used." << std::endl;
+            cerr << "Warning: Invalid vector size for Landmark constructor. Default values used." << std::endl;
         }
     }
 
     // Function to print the coordinates
     void print()  {
-        std::cout << "(" << x << ", " << y << ", " << z << ")" << std::endl;
+        cout << "(" << x << ", " << y << ", " << z << ")" << endl;
     }
 
     vector<float> toVector() {
-        vector<float> landmark_vector;
-        
+        vector<float> landmark_vector; 
         landmark_vector.push_back(x);
         landmark_vector.push_back(y);
         landmark_vector.push_back(z);
         return landmark_vector;   
-
     }
-
 };
 
 /*
@@ -69,26 +68,25 @@ Landmark(const std::vector<double>& point) {
  * @double x
  * @double y
  * @double z
- 
+ */
 class Hand_Landmarks {
-    private:
-    vector<Landmark> landmarks(21);
+    public:
+    vector<Landmark> landmarks;
     //int handedness;
+    
+    // Constructor to initialize the vector with 21 default Landmark objects
+    Hand_Landmarks() : landmarks(21, Landmark()) {}
+    // Constructor to initialize the vector with specific Landmark objects
+    Hand_Landmarks(const vector<Landmark>& initialLandmarks) : landmarks(initialLandmarks) {}
 
-    public: 
-    //Construct with vector
-    Hand_Landmark(vector<Point>& landmarks) {
-        assert(landmarks.size()==21)
-        this->landmarks=landmarks;
-        
-    }
-    //Default constructor
-    Hand_Landmark() {
-
-        this->landmarks.resize(21,0);
-        
+    // Print function to display the contents of Hand_Landmarks
+    void print() const {
+        for (const Landmark& landmark : landmarks) {
+            cout << "(" << landmark.x << ", " << landmark.y << ", " << landmark.z << ")" << endl;
+        }
     }
 };
+
 
 /*
  * @brief Stores possible names and actions for "labels" for images
